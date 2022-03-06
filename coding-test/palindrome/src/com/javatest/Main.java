039d1d27cb9d5758347e984a85d9e872a6c60054
Bruce Han <intrager@naver.com>
2022-03-07 01:32:29 +0900

    palindrome

---
+++ coding-test/palindrome/src/com/javatest/Main.java
@@ -0,0 +1,25 @@
+package com.javatest;
+
+import java.util.Scanner;
+
+public class Main {
+    public String solution(String str) {
+        String answer = "NO";
+        String tmp = new StringBuilder(str).reverse().toString();
+        if(str.equalsIgnoreCase(tmp)) answer="YES";
+
+//        str = str.toUpperCase();
+//        int len = str.length();
+//        for(int i = 0; i < len/2; i++) {
+//            if(str.charAt(i) != str.charAt(len-i-1)) return "NO";
+//        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+	    Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.print(T.solution(str));
+    }
+}
