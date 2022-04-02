30591790a83b99c69798ec43971f4058f04c20e5
Bruce Han <intrager@naver.com>
2022-04-03 00:35:55 +0900

    프로그래밍 3대 요소 학습, 객체 실습

---
+++ basic/JAVA-TPC/src/TPC01.java
@@ -0,0 +1,8 @@
+
+public class TPC01 {
+	public static void main(String[] args) {
+		
+		// syso + Ctrl키 + spaceBar키
+		System.out.println("Hello Java");
+	}
+}
---
+++ basic/JAVA-TPC/src/TPC02.java
@@ -0,0 +1,27 @@
+
+public class TPC02 {
+	public static void main(String[] args) {
+		// 프로그래밍의 3대 요소 : 변수, 자료형(Data Type), 할당(=)
+		// 1 + 1 = 2
+		int a, b, c;
+		a = 1;
+		b = 1;
+		c = a + b;
+		System.out.println(c);
+		
+		float f;	// float, double
+		f = 34.5f;
+		System.out.println(f);
+		
+		char d;
+		d='A';	// 쌍따옴표는 char로 못 묶는다.
+		System.out.println(d);
+		
+		boolean g;
+		g=true;	// false(거짓), true(참)
+		
+		System.out.println(g);
+		
+		
+	}
+}
