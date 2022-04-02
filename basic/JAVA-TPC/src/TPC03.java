33ebee46e5729f86fd345f3f3192b4435a38c623
Bruce Han <intrager@naver.com>
2022-04-03 03:23:54 +0900

    Variable vs Array Relationships

--- basic/JAVA-TPC/src/TPC03.java
+++ basic/JAVA-TPC/src/TPC03.java
@@ -1,4 +1,5 @@
 import kr.bit.Book;
+import kr.bit.PersonVO;
 
 public class TPC03 {
 	public static void main(String[] args) {
@@ -9,5 +10,26 @@ public class TPC03 {
 		
 		// 책 1권을 저장하기 위한 변수를 선언하시오.
 		Book b;
+		b = new Book();	// () 생성자 메서드
+		// 실체 -> Instance, 인스턴스 ==> b는 객체(변수)
+		b.title="자바";
+		b.price=15000;
+		b.company="한빛미디어";
+		b.page=700;
+		
+		System.out.println(b.title + " " + b.price + " " + b.company + " " + b.page);
+		
+		PersonVO p;
+		p = new PersonVO();
+		p.name="brucehan";
+		p.age=26;
+		p.weight=68.9;
+		p.height=178.7;
+		
+		System.out.print(p.name + "\t");
+		System.out.print(p.age + "\t");
+		System.out.print(p.weight + "\t");
+		System.out.println(p.height + "\t");
+		
 	}
 }
---
+++ basic/JAVA-TPC/src/kr/bit/PersonVO.java
@@ -0,0 +1,9 @@
+package kr.bit;
+// 회원 - 이름, 나이, 몸무게, 키
+public class PersonVO {
+	// 상태 정보, 속성, 멤버변수
+	public String name;
+	public Integer age;
+	public Double weight;
+	public Double height;
+}
