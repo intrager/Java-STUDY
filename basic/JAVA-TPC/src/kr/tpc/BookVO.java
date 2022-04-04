12929092e44b1b98897bbe26088f624f609bb48c
Bruce Han <intrager@naver.com>
2022-04-04 21:41:47 +0900

    How objects are created in memory

--- basic/JAVA-TPC/src/kr/tpc/BookVO.java
+++ basic/JAVA-TPC/src/kr/tpc/BookVO.java
@@ -6,4 +6,18 @@ public class BookVO {
 	public String company;
 	public int page;
 	// 디폴트 생성자 메서드 생략돼있음
+	public BookVO() {
+		// 초기화 작업
+		title="자바";
+		price=14000;
+		company="이지스";
+		page=700;
+	}
+	// 생성자 메서드의 중복정의(Overloading)
+	public BookVO(String title, int price, String company, int page) {
+		this.title = title;
+		this.price = price;
+		this.company = company;
+		this.page = page;
+	}
 }
