b27e26bbc942432519d02d405425bc1a376c7b1c
Bruce Han <intrager@naver.com>
2022-04-06 23:19:47 +0900

    Override

---
+++ basic/JAVA-TPC/src/kr/tpc/Animal.java
@@ -0,0 +1,8 @@
+package kr.tpc;
+
+public class Animal {
+	// Dog, Cat --> eat()
+	public void eat() {
+		System.out.println("고양이처럼 먹다");	// 포괄적, 추상적
+	}
+}
---
+++ basic/JAVA-TPC/src/kr/tpc/Cat.java
@@ -0,0 +1,7 @@
+package kr.tpc;
+
+public class Cat extends Animal{
+	public void night() {
+		System.out.println("밤에 눈에도 빛이 난다.");
+	}
+}
