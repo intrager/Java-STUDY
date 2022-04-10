98847af853c877005779fa8f6902d77ddf2fc170
Bruce Han <intrager@naver.com>
2022-04-10 13:54:48 +0900

    Learn interface by way of RemoCon example

--- basic/JAVA-TPC/src/kr/poly/RemoCon.java
+++ basic/JAVA-TPC/src/kr/poly/RemoCon.java
@@ -1,9 +1,9 @@
 package kr.poly;
 
 public interface RemoCon {	// 객체 생성 안 됨
-	// final static이 붙어있는 상수 사용 가능
+	// 얘네는 본디 final static이 붙어있는 상수로 사용 가능
 	int MaxCh = 100;	// public static final int MaxCh
-	int MinCh = 1;		// public static final int MinCh
+	public final static int MinCh = 1;		// public static final int MinCh
 	// 추상메서드
 	public void chUp();	// abstract가 생략되어있음(묵시적임)
 	public void chDown();
--- basic/JAVA-TPC/src/kr/poly/TV.java
+++ basic/JAVA-TPC/src/kr/poly/TV.java
@@ -1,15 +1,27 @@
 package kr.poly;
 
 public class TV implements RemoCon {
-
+	int currCh = 70;
 	@Override
 	public void chUp() {
-		System.out.println("TV의 채널이 올라간다.");		
+		if(currCh < RemoCon.MaxCh) {
+			currCh++;
+			System.out.println("TV의 채널이 올라간다. : " + currCh);		
+		} else {
+			currCh = 1;
+			System.out.println("TV의 채널이 올라간다. : " + currCh);
+		}
 	}
 
 	@Override
 	public void chDown() {
-		System.out.println("TV의 채널이 내려간다.");
+		if(currCh > RemoCon.MinCh) {
+			currCh--;
+			System.out.println("TV의 채널이 내려간다. : " + currCh);
+		} else {
+			currCh = 100;
+			System.out.println("TV의 채널이 내려간다. : " + currCh);
+		}
 	}
 
 	@Override
