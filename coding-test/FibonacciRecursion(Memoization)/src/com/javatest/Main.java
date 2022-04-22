e6dd430f240177f7d7f903f882554f7456364c62
Bruce Han <intrager@naver.com>
2022-04-22 23:53:48 +0900

    FibonacciRecursion(Memoization)3

--- coding-test/FibonacciRecursion(Memoization)/src/com/javatest/Main.java
+++ coding-test/FibonacciRecursion(Memoization)/src/com/javatest/Main.java
@@ -3,9 +3,11 @@ package com.javatest;
 import java.util.Scanner;
 
 public class Main {
-
+    // n = 45 기준 첫 번째에 비해 두 번째는 10배정도 빨라짐.
+    // 세 번째에서는 거의 1초만에 나옴 (세 번째 : Memoization)
     static int[] fibo;
     public int depthFirstSearch(int n) {
+        if(fibo[n] > 0) return fibo[n];
         if(n==1) return fibo[n] = 1;
         else if (n==2) return fibo[n] = 1;
         else return fibo[n] = depthFirstSearch(n-2) + depthFirstSearch(n-1);
