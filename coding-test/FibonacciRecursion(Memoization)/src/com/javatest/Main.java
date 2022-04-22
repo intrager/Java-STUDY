74ff96ee2df2976e2bd915e40c307ae11eedc72d
Bruce Han <intrager@naver.com>
2022-04-22 23:27:50 +0900

    FibonacciRecursion(Memoization)

--- coding-test/FibonacciRecursion(Memoization)/src/com/javatest/Main.java
+++ coding-test/FibonacciRecursion(Memoization)/src/com/javatest/Main.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int[] fibo;
     public int depthFirstSearch(int n) {
         if(n==1) return 1;
         else if (n==2) return 1;
-        else return depthFirstSearch(n-2) + depthFirstSearch(n-1);
+        else return fibo[n] = depthFirstSearch(n-2) + depthFirstSearch(n-1);
     }
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        for(int i = 1; i < n; i++) System.out.println(T.depthFirstSearch(i) + " ");
+        fibo = new int[n+1];
+        T.depthFirstSearch(n);
+        for(int i = 1; i <= n; i++) System.out.print(fibo[i] + " ");
     }
 }
