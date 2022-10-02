c7113261f30ab7f928fd5e53cbd9c47548ae5d91
Bruce Han <intrager@naver.com>
2022-10-03 00:26:04 +0900

    tomato

--- coding-test/tomato/src/com/javatest/Main.java
+++ coding-test/tomato/src/com/javatest/Main.java
@@ -36,16 +36,32 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        m = kb.nextInt();
-        n = kb.nextInt();
+        m = kb.nextInt(); //열
+        n = kb.nextInt(); //행
         board = new int[n][m];
         dis = new int[n][m];
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+            for(int j = 0; j < m; j++) { //토마토의 정보 읽음
                 board[i][j] = kb.nextInt();
-                if(board[i][j] == 1) Q.offer(new Point(i, j));
+                if(board[i][j] == 1) Q.offer(new Point(i, j)); //익은 토마토의 위치, 미리 출발점들 다 넣어놔라
             }
         }
         T.BFS();
+        boolean flag = true; //익지 않을 수도 있다
+        int answer = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(board[i][j] == 0) flag = false; //익지 않은 토마토가 있다? -> -1 출력
+            }
+        }
+        if(flag) { //다 익었다?
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < m; j++) {
+                    answer = Math.max(answer, dis[i][j]); //최댓값 answer에 삽입
+                }
+            }
+            System.out.println(answer);
+        } else
+            System.out.println(-1);
     }
 }
