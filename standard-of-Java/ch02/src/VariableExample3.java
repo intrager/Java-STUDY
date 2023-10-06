71fa2894d18625914f30844e2694ecff6f28af62
intrager <intrager@naver.com>
2023-10-07 04:21:23 +0900

    constant and literal

---
+++ standard-of-Java/ch02/src/VariableExample3.java
@@ -0,0 +1,8 @@
+public class VariableExample3 {
+    public static void main(String[] args) {
+        final int score; // = 100;
+        score = 200;
+
+        System.out.println(score);
+    }
+}
