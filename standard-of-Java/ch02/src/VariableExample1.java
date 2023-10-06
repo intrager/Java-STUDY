473f6c339bc6135609c4a4af617122ee6ea6ac2f
intrager <intrager@naver.com>
2023-10-07 04:20:49 +0900

    learn about variable

---
+++ standard-of-Java/ch02/src/VariableExample1.java
@@ -0,0 +1,10 @@
+public class VariableExample1 {
+    public static void main(String[] args) {
+//        System.out.println("6 + 3");
+        int x = 4, y = 2;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+    }
+}
