b70fda95a606fe5ba2a6b1f6e6891ab33b1bec87
intrager <intrager@naver.com>
2023-10-08 13:24:51 +0900

    Literal type and suffix

---
+++ standard-of-Java/ch02/src/VariableExample4.java
@@ -0,0 +1,23 @@
+public class VariableExample4 {
+    public static void main(String[] args) {
+        final int score; // = 100;
+        score = 200;
+        boolean power = true;
+
+        byte b = 127;   // -128 ~ 127
+
+        int oct = 010;  // 8진수, 10진수 8
+        int hex = 0x10; // 16진수, 10진수 16
+
+        System.out.println(oct + " " + hex);
+
+        long l = 10_000_000_000L;
+
+        float f = 3.14f;
+        double d = 3.14;
+        System.out.println(10.);
+        System.out.println(.10);
+        System.out.println(10f);
+        System.out.println(1e3);
+    }
+}
